694a65ee08fff1f9cb1df3b96fa17b0259619c10
princeorji <michaelorji8039@gmail.com>
2025-08-09 18:30:14 +0100

    initial

---
+++ src/models/user.js
@@ -0,0 +1,34 @@
+const { DataTypes, Model } = require('sequelize')
+const sequelize = require('../config/db')
+
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true   
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false   
+        }
+    },
+    {
+        sequelize,
+        modelName: 'users'
+    }
+)
+
+module.exports = User
